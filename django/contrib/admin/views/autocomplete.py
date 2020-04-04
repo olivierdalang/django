ae87f9c5ebca71745a3d38598556ca071c0fa2e9
olivierdalang <olivier.dalang@gmail.com>
2020-04-05 00:11:50 +0200

    ...

--- django/contrib/admin/views/autocomplete.py
+++ django/contrib/admin/views/autocomplete.py
@@ -30,7 +30,7 @@ class AutocompleteJsonView(BaseListView):
         context = self.get_context_data()
         return JsonResponse({
             'results': [
-                {'id': str(obj.pk), 'text': str(obj), 'safe': isinstance(obj, SafeString)}
+                {'id': str(obj.pk), 'text': str(obj), 'safe': isinstance(str(obj), SafeString)}
                 for obj in context['object_list']
             ],
             'pagination': {'more': context['page_obj'].has_next()},
